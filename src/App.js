50ea66e884f66bc72c821509fa5bf85d13b1c028
SravanKumar35 <kumarsravan9182@gmail.com>
2020-07-11 14:13:00 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import HelloWorld from './components/HelloWorld'
+import CounterExample from './components/CounterExample';
 
 function App() {
   return (
     <div >
       <HelloWorld name="Asra" />
+      <CounterExample/>
     </div>
   );
 }
